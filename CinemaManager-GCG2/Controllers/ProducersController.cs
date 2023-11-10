1166c68d0296443296eadd7080446b058e78b32b
firas hamila <firas.hm.1996@gmail.com>
2023-11-10 14:44:05 +0100

    Ajoutez des fichiers projet.

---
+++ CinemaManager-GCG2/Controllers/ProducersController.cs
@@ -0,0 +1,95 @@
+﻿using CinemaManager_GCG2.Models.Cinema;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaManager_GCG2.Controllers
+{
+    public class ProducersController : Controller
+    {
+        CinemaDbGcg2Context _context;
+        public ProducersController(CinemaDbGcg2Context context)
+        {
+            _context = context;
+        }
+        // GET: ProducersController
+        public ActionResult Index()
+        {
+            return View(_context.Producers.ToList());
+        }
+
+        // GET: ProducersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(_context.Producers.Find(id));
+        }
+
+        // GET: ProducersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProducersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Producer p)
+        {
+            try
+            {
+                _context.Producers.Add(p);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProducersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(_context.Producers.Find(id));
+        }
+
+        // POST: ProducersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Producer prod)
+        {
+            try
+            {
+                _context.Producers.Update(prod);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProducersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(_context.Producers.Find(id));
+        }
+
+        // POST: ProducersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Producer prod)
+        {
+            try
+            {
+                _context.Producers.Remove(prod);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CinemaManager-GCG2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CinemaManager_GCG2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
